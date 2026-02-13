b5f637d7384254587ec8fab81a0465797dead575
Ayush Choudhary <ayushchoudhary18481@gmail.com>
2026-02-13 16:14:34 +0530

    Final clean repo setup

--- server/routes/messRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { scanQRCode } = require('../controllers/messController');
-
-// The Scanner will hit this URL: http://localhost:5000/api/mess/scan
-router.post('/scan', scanQRCode);
-
-module.exports = router;
